f3f932cbdd86a93e3d9314ba9dd84dbf05138e94
Felipe Bochini <bochinifelipe@gmail.com>
2024-04-21 21:48:48 -0300

    att do JS e admin
    adição do javascript para página do ADMIN e atualização do PHP do admin.php
    
    Co-Authored-By: BielFuriato <128671129+BielFuriato@users.noreply.github.com>

---
+++ back-end/conexão/add_candidato.php
@@ -0,0 +1,41 @@
+
+<?php
+
+$nome = $_POST['nome'];
+$email = $_POST['email'];
+$cpf = $_POST['cpf'];
+$telefone = $_POST['telefone'];
+$curriculo = $_FILES['curriculo'];
+
+
+$strcon = mysqli_connect("localhost", "root", "", "sunsage_db") or die("Erro ao conectar com o banco");
+
+// Inserir os dados do candidato na tabela candidato usando instrução preparada
+
+$pasta = "curriculos/";
+$nomeDoCurriculo = $curriculo["name"];
+$novoNomeDoCurriculo = uniqid();
+$extensao = strtolower(pathinfo($nomeDoCurriculo, PATHINFO_EXTENSION));
+
+if ($extensao != "pdf")
+    die("Tipo de arquivo não aceito");
+
+$path = $pasta . $novoNomeDoCurriculo . "." . $extensao;
+
+$deu_certo = move_uploaded_file($curriculo["tmp_name"], $path);
+if ($deu_certo) {
+    echo "<p>Currículo enviado com sucesso";
+} else
+    echo "Falha ao enviar o arquivo<br>";
+    echo "Erro: " . $_FILES['curriculo']['error'];
+
+$nome = trim($cpf);
+
+$sql_candidato = "INSERT INTO candidato (nome, email, CPF, numero_telefone, paths) VALUES (?, ?, ?, ?, ?)";
+$stmt_candidato = $strcon->prepare($sql_candidato);
+$stmt_candidato->bind_param("ssiss", $nome, $email, $cpf, $telefone, $path);
+$stmt_candidato->execute();
+$stmt_candidato->close();
+
+?>
+    
